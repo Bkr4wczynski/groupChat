ab642e789b0f80d8a464ddb4f66d16c7b9547999
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-26 12:01:50 +0200

    created simple echo server

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.bartek.groupchat {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.bartek.groupchat to javafx.fxml;
+    exports com.bartek.groupchat;
+}
