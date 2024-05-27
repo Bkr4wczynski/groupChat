334cac9854dcd9f0a66f591147d81c9f1eb67ff4
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-27 15:51:19 +0200

    you can now redirect to chat page

---
+++ src/main/java/com/bartek/groupchat/user/app/controllers/chatController.java
@@ -0,0 +1,4 @@
+package com.bartek.groupchat.user.app.controllers;
+
+public class chatController {
+}
--- src/main/java/com/bartek/groupchat/user/app/controllers/entryController.java
+++ src/main/java/com/bartek/groupchat/user/app/controllers/entryController.java
@@ -1,24 +1,42 @@
 package com.bartek.groupchat.user.app.controllers;
 
+import com.bartek.groupchat.user.app.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class entryController {
     public TextField nameEntry;
     public Label warningsLabel;
 
-    public void enterGroupChat(){
+    public void enterGroupChat(ActionEvent event){
         String enteredName = nameEntry.getText();
         if (!validateUsername(enteredName)){
             warningsLabel.setText("Username is not valid");
             return;
         }
-        System.out.println("Entering");
+        try {
+            redirectToChatPage(event);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void clearWarningsLabel(){
         warningsLabel.setText("");
     }
     private boolean validateUsername(String username){
         return username.matches("[$a-zA-Z0-9-_]{3,19}");
+    }
+    private void redirectToChatPage(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(FXMLLoader.load(App.class.getResource("/com/bartek/groupchat/FXML/chat.fxml")));
+        stage.setScene(scene);
+        stage.show();
     }
 }
