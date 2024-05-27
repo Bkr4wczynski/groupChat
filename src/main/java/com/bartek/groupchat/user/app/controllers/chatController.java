657e6d2c4f38cfe333b2e7d2793618b673d6200a
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-27 16:06:13 +0200

    simple gui without actual functionality

--- src/main/java/com/bartek/groupchat/user/app/controllers/chatController.java
+++ src/main/java/com/bartek/groupchat/user/app/controllers/chatController.java
@@ -1,4 +1,20 @@
 package com.bartek.groupchat.user.app.controllers;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class chatController {
+    public TextField messageEntry;
+    public TextArea messagesArea;
+
+    public void sendMessage(){
+        String message = messageEntry.getText();
+        messageEntry.setText("");
+        System.out.println(message);
+        receiveMessage();
+    }
+    public void receiveMessage(){
+        String message = "Example";
+        messagesArea.setText(messagesArea.getText()+"\n"+message);
+    }
 }
