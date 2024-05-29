8609c710892b8a5091d56411940e007538d9f3ee
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-29 18:57:33 +0200

    cli now shutdowns if server is off

--- src/main/java/com/bartek/groupchat/user/app/CLI_GroupChat.java
+++ src/main/java/com/bartek/groupchat/user/app/CLI_GroupChat.java
@@ -4,13 +4,21 @@ import com.bartek.groupchat.utils.AppType;
 import com.bartek.groupchat.user.client.Client;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.util.Scanner;
 
 public class CLI_GroupChat {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Client client = new Client(InetAddress.getByName("localhost"), AppType.CLI);
+        Client client = null;
+        try{
+            client = new Client(InetAddress.getByName("localhost"), AppType.CLI);
+        }
+        catch (ConnectException e){
+            System.out.println("Server is offline!");
+            System.exit(0);
+        }
         String input;
         System.out.println("Enter your username: ");
         String username = reformatUsername(scanner.nextLine());
